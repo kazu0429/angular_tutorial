69bed5b731c0e0db325ca2c384110b69e060ab17
kazu0429 <k.shouhi0412@i.softbank.jp>
2024-02-18 22:41:53 +0900

    add lesson5, 6, 7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,8 +17,8 @@ import { Housinglocation } from '../housinglocation';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-    <section>
-      <app-housing-location></app-housing-location>
+    <section class="results">
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
